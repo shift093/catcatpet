62f5e678dccf58439e23b9ad278774b6f8695aab
Setup <33972291+shift093@users.noreply.github.com>
2019-12-09 04:49:25 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,9 +5,8 @@ import urllib
 sched = BlockingScheduler()
 
 # 定義排程 : 每 1 分鐘就做一次 def scheduled_jog()
-sched.add_job(scheduled_job,'cron', day_of_week='mon-sun', minute='*/1')
 def scheduled_job():
     url = "https://catcattest.herokuapp.com/"
     connect = urllib.request.urlopen(url)
-    
+sched.add_job(scheduled_job,'cron', day_of_week='mon-sun', minute='*/1')
 sched.start()  # 啟動排程
