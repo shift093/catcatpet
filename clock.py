a34c42ee0489dc3107a1e1407ce388e1173529f2
Setup <33972291+shift093@users.noreply.github.com>
2019-12-09 21:25:05 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ import urllib.request
 sched = BlockingScheduler()
 
 # 定義排程 : 每 1 分鐘就做一次 def scheduled_jog()
-"""
+
 def scheduled_job():
     url = "https://catcattest.herokuapp.com/"
     connect = urllib.request.urlopen(url)
@@ -20,12 +20,13 @@ def lineNotifyMessage(token, msg):
     massage = {'message': msg}
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = massage)
     return r.status_code
-
+"""
     
 if __name__ == '__main__':
     message = '[LINE Notify] Hello World 記得抓貓唷' # 要傳送的訊息內容
     token = 'nHiUiakxNdMzf9Kt05A1oWJTas9oZQ5Oa2gYF5bx5AK' # 權杖值
-    sched.add_job(lineNotifyMessage(token, message),'cron', day_of_week='mon-sun', minute='*/1')
+    #sched.add_job(lineNotifyMessage(token, message),'cron', day_of_week='mon-sun', minute='*/1')
+    sched.add_job(scheduled_job,'cron', day_of_week='mon-sun', minute='*/1')
     #sched.add_job(scheduled_job,'cron',hour='9-10,13-16,18-20',minute=58)
     sched.start()  # 啟動排程
 
