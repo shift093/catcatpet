4e9c69a76c0d4b2310f020539db217581afaf38b
Setup <33972291+shift093@users.noreply.github.com>
2019-12-09 22:15:22 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -14,7 +14,8 @@ def scheduled_job():
 def lineNotifyMessage():
     message = '[測試階段]Hello！各位～記得抓貓唷！'
     #message = '[LINE Notify]Hello！各位～記得抓貓唷！' # 要傳送的訊息內容
-    token = 'nHiUiakxNdMzf9Kt05A1oWJTas9oZQ5Oa2gYF5bx5AK' # 權杖值
+    #token = 'nHiUiakxNdMzf9Kt05A1oWJTas9oZQ5Oa2gYF5bx5AK' # 個人權杖值
+    token = 'TZuyoNARaeVyfDvqw7S2lnAMyCXnr0exB0wsxPhYx5M'  #群組權杖值
     headers = {
         "Authorization": "Bearer " + token, 
         "Content-Type" : "application/x-www-form-urlencoded"
@@ -31,11 +32,11 @@ if __name__ == '__main__':
     #sched.add_job(lineNotifyMessage(token, message),'cron', day_of_week='mon-sun', minute='*/1')
     #sched.add_job(lineNotifyMessage,'cron', day_of_week='mon-sun', minute='*/1')
     
-    sched.add_job(lineNotifyMessage,'cron',hour='1-23',minute=5)
-    sched.add_job(lineNotifyMessage,'cron',hour='1-23',minute=6)
+    #sched.add_job(lineNotifyMessage,'cron',hour='1-23',minute=5)#測試
+    #sched.add_job(lineNotifyMessage,'cron',hour='1-23',minute=6)#測試
     
-    #sched.add_job(lineNotifyMessage,'cron',hour='1-2,5-8,10-12',minute=4)
-    #sched.add_job(lineNotifyMessage,'cron',hour='1-2,5-8,10-12',minute=5)
+    sched.add_job(lineNotifyMessage,'cron',hour='1-2,5-8,10-12',minute=58)
+    sched.add_job(lineNotifyMessage,'cron',hour='1-2,5-8,10-12',minute=59)
     
     #sched.add_job(scheduled_job,'cron', day_of_week='mon-sun', minute='*/1')
     #sched.add_job(scheduled_job,'cron',hour='9-10,13-16,18-20',minute=58)
