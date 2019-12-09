0215b79ccaebc9ccb9d7b1632f196d4a81990c52
Setup <33972291+shift093@users.noreply.github.com>
2019-12-09 21:53:30 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -18,8 +18,8 @@ def lineNotifyMessage():
         "Authorization": "Bearer " + token, 
         "Content-Type" : "application/x-www-form-urlencoded"
     }
-
-    massage = {'message': msg}
+    massage = {'message': message}
+    #massage = {'message': msg}
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = massage)
     return r.status_code
 
